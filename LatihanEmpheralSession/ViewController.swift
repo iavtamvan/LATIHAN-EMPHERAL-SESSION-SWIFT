f172a503fdccf4a35159ff1dcb6a7cf10f771bc2
Ade Fajr Ariav <ade.fajr.ariav@gmail.com>
2023-02-07 08:55:24 +0700

    Initial Commit

---
+++ LatihanEmpheralSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LatihanEmpheralSession
+//
+//  Created by Ade Fajr Ariav on 07/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
